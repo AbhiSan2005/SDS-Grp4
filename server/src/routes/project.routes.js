f4bb8bff8d0e6d4095f71c5d7c13fa85c4554ba8
Abhiraj Sankpal <sankpalabhiraj1@gmail.com>
2025-10-16 16:35:00 +0530

    feat: faculty + report models + frontend portal for faculty advisor

--- server/src/routes/project.routes.js
+++ server/src/routes/project.routes.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import { getProjects, createProject, updateProject, deleteProject } from '../controllers/project.controller.js';
+import { protect } from '../middleware/auth.middleware.js';
+import { adminOnly } from '../middleware/role.middleware.js';
 
 const router = express.Router();
 
+// Public route - anyone can view projects
 router.get('/', getProjects);
 
-router.post('/', createProject);
-
-router.put('/:id', updateProject);
-
-router.delete('/:id', deleteProject);
+// Protected routes - Admin only
+router.post('/', protect, adminOnly, createProject);
+router.put('/:id', protect, adminOnly, updateProject);
+router.delete('/:id', protect, adminOnly, deleteProject);
 
 export default router;
